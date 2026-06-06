7967e7f0fb5c49fe00d145c12b5f6b465f89fcc8
DomeniGeco <domenigeco@gmail.com>
2026-06-06 09:26:20 +0200

    Update run_js.py

--- run_js.py
+++ run_js.py
@@ -7,24 +7,32 @@ from require import require
 init()
 
 def setcolor(color):
-    color = color.upper()
-    return getattr(Fore, color)
+    return getattr(Fore, color.upper())
 
 def ripristina():
     return Style.RESET_ALL
+
+
+context = js2py.EvalJs({})
+
+context.require = require
+
 
 file = sys.argv[1]
 
 with open(Path(file), "r", encoding="utf-8") as f:
     filec = f.read()
 
+
 try:
-    risultato = js2py.eval_js(filec)
+    risultato = context.execute(filec)
+
+    if risultato is not None:
+        print(risultato)
+
 except Exception as e:
     print(setcolor("RED") + "Errore nell'esecuzione di:" + ripristina())
     print("   " + file)
     print("Messaggio Errore:")
     print("    " + str(e))
     print(ripristina())
-else:
-    print(risultato)
